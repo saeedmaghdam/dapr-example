df7f44cded52d1e2348fb1c920ed68feea129a8b
saeedmaghdam <smasafat@gmail.com>
2023-01-29 17:29:23 +0100

    init: initialized project

---
+++ dapr-example-frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dapr_example_frontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get,
+                "dapr-example-backend",
+                "weatherforecast");
+
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
---
+++ dapr-example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapr.Client;
+
+const string storeName = "statestore";
+const string key = "counter";
+
+var daprClient = new DaprClientBuilder().Build();
+var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+    Console.WriteLine($"Counter = {counter++}");
+
+    await daprClient.SaveStateAsync(storeName, key, counter);
+    await Task.Delay(1000);
+}
