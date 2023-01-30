f1e717bd578eb873edfc49f54a3351949a0d1d88
saeedmaghdam <smasafat@gmail.com>
2023-01-30 15:02:16 +0100

    feat: wait for sidecar to be loaded

--- dapr-example/Program.cs
+++ dapr-example/Program.cs
@@ -4,7 +4,10 @@ using Dapr.Client;
 const string storeName = "statestore";
 const string key = "counter";
 
+Console.WriteLine($"Process started ...");
+
 var daprClient = new DaprClientBuilder().Build();
+await daprClient.WaitForSidecarAsync();
 var counter = await daprClient.GetStateAsync<int>(storeName, key);
 
 while (true)
